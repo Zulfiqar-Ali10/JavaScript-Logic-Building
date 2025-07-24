0ce9ea15dac05b1ecc70c37bd3713607605430e4
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-07-24 16:14:45 +0500

    changes-class03

--- Class_03/app.js
+++ Class_03/app.js
@@ -3,12 +3,16 @@
 // Write a function that return the reverse of a string 
 
 function reverseAString(inputString) {
+    let result = "";
     for(let i = inputString.length - 1; i >= 0; i--){
-        console.log(inputString[i]);
+        // console.log(inputString[i]);
+        result = result + inputString[i];
     }
+    return result;
 }
 
-console.log(reverseAString("Zulfiqar"));
+console.log(reverseAString("Zulfiqar")); 
+console.log(reverseAString("Zulfiqar Ecochamp backend developer")); 
 
 
 //  problem statement 6 

ff6f21fa5be07c7e96d85771358b0a490bbaaf63
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-07-24 16:32:31 +0500

    changes-class_03

--- Class_03/app.js
+++ Class_03/app.js
@@ -3,16 +3,42 @@
 // Write a function that return the reverse of a string 
 
 function reverseAString(inputString) {
+
+    if(typeof inputString !== "string"){
+        throw new Error("Only strings are allowed");
+    }
     let result = "";
     for(let i = inputString.length - 1; i >= 0; i--){
         // console.log(inputString[i]);
-        result = result + inputString[i];
+
+        // result = result + inputString[i];
+        result += inputString[i];
     }
     return result;
 }
 
 console.log(reverseAString("Zulfiqar")); 
 console.log(reverseAString("Zulfiqar Ecochamp backend developer")); 
+
+// console.log(reverseAString(1233)); 
+
+
+
+// Second Way same work according to my idea and easy ...
+
+function reverseAString2(inputString1){
+       const splittedInput = inputString1.split("");
+       const reverseArr = splittedInput.reverse();
+       const joinArr = reverseArr.join("");
+    //    console.log(splittedInput);
+    //    console.log(reverseArr);
+    //    console.log(joinArr);
+    return joinArr
+}
+
+console.log("reverceAString2");
+console.log(reverseAString2("Asad"));
+
 
 
 //  problem statement 6 

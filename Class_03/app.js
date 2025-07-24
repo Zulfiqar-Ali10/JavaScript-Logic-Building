9f9cefb15c64ee88b45dd8d38a0a744ad8fac82e
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-07-24 14:46:18 +0500

    class-03-changes

---
+++ Class_03/app.js
@@ -0,0 +1,14 @@
+//  problem statement 6 
+
+// Write a function that return the reverse of a string 
+
+function reverseAString(inputString) {
+    for(let i = inputString.length - 1; i >= 0; i--){
+        console.log(inputString[i]);
+    }
+}
+
+console.log(reverseAString("Zulfiqar"));
+
+
+//  problem statement 6 

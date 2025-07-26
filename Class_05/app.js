6c66e050a79e5846d618c5f106a20eec9880441c
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-07-26 15:25:27 +0500

    changes-class-05

--- Class_05/app.js
+++ Class_05/app.js
@@ -1,5 +1,24 @@
 //// problem no -7 
 
+// Write a function that tells if provided year is a leap year or not: 
 
+function isLeapYear(year){
+    if(year % 4 === 0){
+        return true;
+    }else{
+        return false
+    }
+}
+
+//////////second way //////
+
+function isLeapYear2(year){
+    return year % 4 === 0
+}
+    
+//////////second way //////
+
+console.log(isLeapYear(2004));
+console.log(isLeapYear2(2004));
 
 //// problem no -7 

1872629a5c230aabe8b30ebe87877a95d6eddcf0
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-08-04 15:14:21 +0500

    changes-Class_15

--- Class_15/app.js
+++ Class_15/app.js
@@ -1,17 +1,48 @@
 // Problem statement 15
 
-function isPrime(inputNumber){
+function isPrime(inputNumber) {
     let result = true;
-    for(let i = 2; i < inputNumber; i++){
-    console.log(inputNumber, "%", i, inputNumber % i);
-    if(inputNumber % i === 0) {
-        result = false;
-        break;
+    for (let i = 2; i < inputNumber; i++) {
+        console.log(inputNumber, "%", i, inputNumber % i);
+        if (inputNumber % i === 0) {
+            result = false;
+            break;
+        }
     }
-    }
+    return result;
 }
+
 
 console.log(isPrime(5));
 console.log(isPrime(4));
+
+
+function isPrime2(inputNumber) {
+    let result = true;
+
+    if(inputNumber === 2){
+        result = true;
+    }
+
+    if (inputNumber % 2 === 0) {
+        result = false;
+    }
+
+    if (result === true) {
+        for (let i = 3; i < inputNumber; i = i + 2) {
+            if (inputNumber % i === 0) {
+                result = false;
+                break;
+            }
+        }
+    }
+    return result;
+
+}
+
+console.log("isPrime2");
+console.log(isPrime2(5));
+console.log(isPrime2(4));
+console.log(isPrime2(97));
 
 // Problem statement 15

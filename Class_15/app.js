1665d5c9821a5b64a5b07b75ecf87bad1cffbe20
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-08-04 14:58:58 +0500

    changes

--- Class_15/app.js
+++ Class_15/app.js
@@ -1,4 +1,17 @@
 // Problem statement 15
 
+function isPrime(inputNumber){
+    let result = true;
+    for(let i = 2; i < inputNumber; i++){
+    console.log(inputNumber, "%", i, inputNumber % i);
+    if(inputNumber % i === 0) {
+        result = false;
+        break;
+    }
+    }
+}
+
+console.log(isPrime(5));
+console.log(isPrime(4));
 
 // Problem statement 15

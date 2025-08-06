8b800ad026982995f1bc1bba9831ac729eb05a0c
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-08-06 20:56:28 +0500

    changes-Class_20

--- Class_20/app.js
+++ Class_20/app.js
@@ -1,6 +1,22 @@
 // Problem Statement 20
 
-// Write a program that checks if a number is ArmStrong or not.
+// Write a program that prints numbers from 1 to N 
+
+function fizzBuzz(inputNumber){
+    for(let i = 1; i <= inputNumber; i++){
+
+        if(i % 3 === 0){
+            console.log("Fizz");
+        }else if(i % 5 === 0){
+            console.log("Buzz");
+        }else{
+            console.log(i);
+            
+        }
+    }
+}
+
+console.log(fizzBuzz(10));
 
 
 

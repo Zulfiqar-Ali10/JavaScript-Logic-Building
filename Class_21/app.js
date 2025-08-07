79f1f09e4df81c27236a9453722949ef812c5137
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-08-07 15:43:54 +0500

    changes-Class_21

--- Class_21/app.js
+++ Class_21/app.js
@@ -1,23 +1,37 @@
-// Problem Statement 20
+// Problem Statement 21
 
-// Write a program that prints numbers from 1 to N 
+// Write a program that prints a number pyramid
 
-function fizzBuzz(inputNumber){
+function generatPyramid(inputNumber){
+    // console.log(inputNumber);
+    
     for(let i = 1; i <= inputNumber; i++){
-
-        if(i % 3 === 0){
-            console.log("Fizz");
-        }else if(i % 5 === 0){
-            console.log("Buzz");
-        }else{
-            console.log(i);
-            
+        let lineContent = "";
+        for(let space = 1; space <= inputNumber - i; space++){
+            lineContent = lineContent + " ";
         }
+
+        for(let count = 1; count <= i; count++){
+            lineContent = lineContent + count;
+        }
+
+        for(let reverseCount = i - 1; reverseCount >= 1; reverseCount--){
+             lineContent = lineContent + reverseCount;
+        }
+
+
+        console.log(lineContent);
+        
     }
+
 }
 
-console.log(fizzBuzz(10));
+console.log(generatPyramid(2));
+console.log(generatPyramid(5));
+// console.log(generatPyramid(10));
 
 
 
-// Problem Statement 20
+
+
+// Problem Statement 21

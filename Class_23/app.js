c74222966481b99d3237d6a54c9d9c51a8455ef2
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-08-11 19:02:27 +0500

    changes-class_13

---
+++ Class_23/app.js
@@ -0,0 +1,27 @@
+// Problem Statement 23
+
+// Write a program that checks if a given string is i alphabetical order.
+
+// console.log("a" .charCodeAt(0));
+
+function isStringInAlphabeticalOrder(inputString){
+    inputString = inputString.replace(/ /g, "");
+    inputString = inputString.toLowerCase();
+    for(let i = 0; i < inputString.length - 1; i++){
+        if(inputString[i] > inputString[i + 1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isStringInAlphabeticalOrder("abcdefg"));
+console.log(isStringInAlphabeticalOrder("abcde fg"));
+console.log(isStringInAlphabeticalOrder("abcdecfg"));
+
+
+
+
+
+
+// Problem Statement 23

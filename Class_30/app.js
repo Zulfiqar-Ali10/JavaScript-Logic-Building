99054d95a639a59aefd6e97a39becfda30e00050
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-08-22 17:04:28 +0500

    class_30

--- Class_30/app.js
+++ Class_30/app.js
@@ -1,5 +1,37 @@
 /// Problem Statement 30
 
+// Write a program  that checks if a given string of parentheses is balanced and well formed.
+
+const parentheses = {
+    '{': '}',
+    '(': ')',
+    '[': ']',
+}
+
+
+console.log(parentheses['{']);
+
+
+
+function isBalanced( inputString ) {
+       console.log( inputString );
+       const parenthesesArray = [];
+       for(let char of inputString){
+        if(parentheses[char]){
+            parenthesesArray.push(char)
+        }else if(char === ')' || char === '}' || char === ']') {
+            const lastOpeningParanthesis = parenthesesArray.pop();
+            if(parentheses[lastOpeningParanthesis] !== char) {
+                return false;
+            }
+        }
+        
+       }
+       return true;
+}
+
+console.log(isBalanced('{([])}'));
+console.log(isBalanced('{([]}'));
 
 
 /// Problem Statement 30

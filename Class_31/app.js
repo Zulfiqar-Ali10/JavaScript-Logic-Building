6a1e9835c292e6a9f0691b0d70f8e4105a69ed86
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-08-23 15:43:50 +0500

    class_31

--- Class_31/app.js
+++ Class_31/app.js
@@ -1,5 +1,24 @@
 /// Problem Statement 31
 
+// Write a program that finds all occurrences of a given word in a big string
+
+const bigString = "This is a big string with the word big repeated in the big string"
+const wordToFind = "big"
+
+// console.log(bigString.indexOf("big"));
+
+
+function findAllOccurences(big, word){
+    const result = [];
+    let index = big.indexOf(word);
+    while(index !== -1){
+        result.push(index);
+        index = big.indexOf(word, index + 1);
+    }
+        console.log(result);
+}
+
+console.log(findAllOccurences(bigString, wordToFind));
 
 
 /// Problem Statement 31

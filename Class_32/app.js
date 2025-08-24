c91d72e2096610d57c0b33c1742ce6643014cf67
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-08-24 14:11:49 +0500

    changes

---
+++ Class_32/app.js
@@ -0,0 +1,23 @@
+/// Problem Statement 32
+
+// Write a program that generates a random number:
+
+
+const maxNumber = 5;
+console.log(Math.floor(Math.random() * maxNumber) + 1);
+let i = 0;
+while(i < 20) {
+    console.log(Math.floor(Math.random() * maxNumber) + 1);
+    i++;
+}
+
+const minimumNumber = 11; // Included
+const maximumNumber = 21; // Dont include
+console.log("Between", minimumNumber, maximumNumber);
+let j = 0;
+while(j < 20){
+    console.log(Math.floor(Math.random() * (maximumNumber - minimumNumber) + minimumNumber));
+    j++;
+}
+
+/// Problem Statement 32

c8adabc628b1993b9a7345736bd5dbd898142eb7
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-08-25 14:16:08 +0500

    Class_33

--- Class_33/app.js
+++ Class_33/app.js
@@ -2,7 +2,28 @@
 
 // Write a program that simulated rolling a dice.
 
+const prompt = require('prompt-sync')();
 
+function rollADice(sidesInADice) {
+    return Math.floor(Math.random() * sidesInADice) + 1;
+}
+
+function rollMultipleDices(numberOfDice, sidesInADice) {
+    const result = [];
+    let counter = 0;
+    while(counter < numberOfDice) {
+        const rollADiceResult = rollADice(sidesInADice);
+        result.push(rollADiceResult)
+        counter++;
+    }
+
+    return result;
+}
+
+const numberOfDices = prompt("Please tell number of dices: ");
+const numberOfSides = prompt("Please tell number of sides in a dice: ");
+
+console.log(rollMultipleDices(numberOfDices, numberOfSides));
 
 
 /// Problem Statement 33
